2a140e0e1831f1ad421e8c0c4220c2bd644e1499
Matt Beanland <beanland@live.com.au>
2025-08-08 23:47:57 +1000

    version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AshOutstanding.MixProject do
   use Mix.Project
 
   @name :ash_outstanding
-  @version "0.2.0"
+  @version "0.2.1"
   @description "Ash resource extension for implementing Outstanding protocol"
   @github_url "https://github.com/diffo-dev/ash_outstanding"
 
